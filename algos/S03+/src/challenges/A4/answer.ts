638aff11478da73c289ac969a91263c7c18cce33
LoicBrassart <loic.brassart@gmail.com>
2025-03-12 09:08:56 +0100

    Add A4 answer

--- algos/S03+/src/challenges/A4/answer.ts
+++ algos/S03+/src/challenges/A4/answer.ts
@@ -8,49 +8,44 @@
  * You have to manipulate dates in vanillaJS. Be carefull to call, if needed, setUTCHours, setUTCMinutes, ...
  * instead of setHouts, setMinutes, ... to avoid timezone offsets!
  *
- * Example:
- * Input: [
- *      { title: "Vélo de course", createdAt: "2023-11-17T11:45:01.721Z" },
- *      { title: "Robot mélangeur", createdAt: "2023-03-12T13:45:01.721Z" },
- *      { title: "Vélo de ville", createdAt: "2023-03-10T07:45:47.721Z" },
- *      { title: "Chaussures (41)", createdAt: "2023-01-21T21:25:47.721Z" },
- *      { title: "Tapis", createdAt: "2023-06-16T21:25:47.721Z" },
- * ]
- * Output: [
- *      {
- *          month: "2023-01-01T00:00:00.000Z",
- *          submissions: [
- *              { title: "Quest Angular", createdAt: "2023-01-21T21:25:47.721Z" }
- *          ]
- *      },
- *      {
- *          month: "2023-03-01T00:00:00.000Z",
- *          submissions: [
- *              { title: "Quest GraphQL", createdAt: "2023-03-12T13:45:01.721Z" },
- *              { title: "Quest ReactJS", createdAt: "2023-03-10T07:45:47.721Z" },
- *          ]
- *      },
- *      {
- *          month: "2023-06-01T00:00:00.000Z",
- *          submissions: [
- *              { title: "Quest MySQL", createdAt: "2023-06-16T21:25:47.721Z" },
- *          ]
- *      },
- *      {
- *          month: "2023-11-01T00:00:00.000Z",
- *          submissions: [
- *              { title: "Quest NodeJS", createdAt: "2023-11-17T11:45:01.721Z" },
- *          ]
- *      },
- * ]
- *
- * @param submissions A list of ads with their title and created datetime
+ * @param ads A list of ads with their title and created datetime
  * @returns A list of objects. Each object must contain a month and its associated ads. The list must be sorted chronoligically
  */
 
 // ↓ uncomment bellow lines and add your response!
-// export default function ({ ads }: { ads: Ad[] }): MonthAds[] {
-// }
+export default function ({ ads }: { ads: Ad[] }): MonthAds[] {
+  // Reduce: "à partir d'un tableau, crée moi une valeur"
+  const monthAdsMap = ads.reduce((acc, ad) => {
+    const date = new Date(ad.createdAt);
+    date.setUTCDate(1);
+    date.setUTCHours(0, 0, 0, 0);
+    const month = date.toISOString();
+
+    // Etat:
+    // - date contient un Date correspondant au début du mois de l'annonce
+    // - month contient une string correspondant au début du mois de l'annonce
+
+    let monthAds = acc.find((ma) => ma.month === month);
+
+    // Si première annonce de ce mois: creer la structure dans acc
+    if (!monthAds) {
+      monthAds = { month, ads: [] };
+      // Etat loop0:
+      // - monthAds contient: { month: "2023-11-01T00:00:00.000Z", ads:[]}
+      acc.push(monthAds);
+    }
+
+    monthAds.ads.push(ad);
+
+    return acc;
+  }, [] as MonthAds[]);
+
+  monthAdsMap.sort(
+    (a, b) => new Date(a.month).getTime() - new Date(b.month).getTime()
+  );
+
+  return monthAdsMap;
+}
 
 // used interfaces, do not touch
 export interface Ad {
